43799de25d73521c26184be04eac80195cf2ae6b
alkihuri <alkihuri@gmail.com>
2026-02-06 16:20:09 +0100

    import fix

--- Blaster/assets/Scripts/Managers/BoardManager.ts
+++ Blaster/assets/Scripts/Managers/BoardManager.ts
@@ -1,3 +1,10 @@
+
+import { ServiceContainer } from '../DI/ServiceContainer';
+import ManagerBase from './ManagerBase';
+import GameConfig  from '../Core/GameConfig';
+
+const { ccclass, property } = cc._decorator;
+
  @ccclass
 export class BoardManager extends ManagerBase {
 
