60a09def688075858b76a66ce3c49d8ab187ba08
alkihuri <alkihuri@gmail.com>
2026-02-08 10:59:13 +0100

    build

---
+++ web-mobile/src/settings.302c2.js
@@ -0,0 +1,23 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "default"
+    ],
+    collisionMatrix: [
+        [
+            true
+        ]
+    ],
+    hasResourcesBundle: false,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/mainscene.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [],
+    bundleVers: {
+        internal: "3e194",
+        main: "af3a4"
+    }
+};
